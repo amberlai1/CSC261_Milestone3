b0b80a71f85863125ba9e0abb37f1eddb8667886
Amber Lai <alai8@u.rochester.edu>
2024-04-07 17:47:32 -0400

    added comments

--- functions.js
+++ functions.js
@@ -7,42 +7,40 @@ function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
 
-// Initialize cart from session storage
+
 let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
 
-// Function to add an item to the cart
+
 function addToCart(itemName, itemPrice, itemImage) {
     
     let existingItem = cart.find(item => item.name === itemName);
 
     if (existingItem) {
-        // If the item already exists, increase its quantity
         existingItem.quantity++
     } else {
-        // If the item doesn't exist, add it to the cart
         const itemId = cart.length + 1;
         cart.push({ id: itemId, name: itemName, price: itemPrice, image: itemImage, quantity: 1 });
     }
     
-    // Update the cart display
+
     updateCart();
     
-    // Store updated cart in session storage
+    // store updated cart in session storage
     sessionStorage.setItem('cart', JSON.stringify(cart));
 }
 
 
 function updateCart() {
-    // Get the cart element
+
     let cartElement = document.getElementById('cart');
     if (cartElement && window.location.pathname.endsWith('shop.html')) {
-    // Clear the existing cart content
+
     cartElement.innerHTML = '';
 
-    // Filter out items with quantity greater than zero
+    //have to be greater than 0 
     let itemsToShow = cart.filter(item => item.quantity > 0);
 
-    // Loop through each item in the cart and add it to the UI
+ 
     itemsToShow.forEach(item => {
         let itemElement = document.createElement('div');
         itemElement.classList.add('grid-item', 'cart-item');
@@ -65,7 +63,7 @@ function updateCart() {
 }
 
 
-// Function to decrease the quantity of an item in the cart
+
 function decreaseQuantity(itemId) {
     let itemIndex = cart.findIndex(item => item.id === itemId);
     if (itemIndex !== -1) {
@@ -80,7 +78,7 @@ function decreaseQuantity(itemId) {
     }
 }
 
-// Function to increase the quantity of an item in the cart
+
 function increaseQuantity(itemId) {
     let itemIndex = cart.findIndex(item => item.id === itemId);
     if (itemIndex !== -1) {
