efbe620cb3b43de2c618c648b5a349dc2b3720b7
Amber Lai <alai8@u.rochester.edu>
2024-04-09 19:16:08 -0400

    initalized csv

--- taskc/load.sql
+++ taskc/load.sql
@@ -1,29 +1,28 @@
--- TODO: assuming these are all csv files -- 
-LOAD DATA LOCAL INFILE '--TODO: insert file path---' INTO TABLE Customer
+LOAD DATA LOCAL INFILE 'taskc/customer.csv' INTO TABLE Customer
   FIELDS TERMINATED BY ',' 
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (customerID, email, phoneNumber, firstName, lastName);
 
-LOAD DATA LOCAL INFILE '--TODO: insert file path---' INTO TABLE Distributor
+LOAD DATA LOCAL INFILE 'taskc/distributor.csv' INTO TABLE Distributor
   FIELDS TERMINATED BY ',' 
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (companyName, distributorID);
 
-LOAD DATA LOCAL INFILE '--TODO: insert file path---' INTO TABLE Transaction
+LOAD DATA LOCAL INFILE 'taskc/transaction.csv' INTO TABLE Transaction
   FIELDS TERMINATED BY ',' 
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (transactionID, customerID, itemID, distributorID);
 
-LOAD DATA LOCAL INFILE '--TODO: insert file path---' INTO TABLE Item
+LOAD DATA LOCAL INFILE 'taskc/item.csv' INTO TABLE Item
   FIELDS TERMINATED BY ',' 
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (name, itemID, price, distributorID, itemType);
 
-LOAD DATA LOCAL INFILE '--TODO: insert file path---' INTO TABLE Availability
+LOAD DATA LOCAL INFILE 'taskc/availability.csv' INTO TABLE Availability
   FIELDS TERMINATED BY ',' 
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
